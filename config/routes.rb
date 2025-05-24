fd2cf9c9076508f2b94412a4f771b37e985344e9
NicolasGoarant <nicolas.goarant@hotmail.fr>
2025-05-24 16:26:55 +0200

    banner

--- config/routes.rb
+++ config/routes.rb
@@ -1,29 +1,30 @@
 Rails.application.routes.draw do
   # Page d'accueil -> Dashboard si connecté, Login si non connecté
   root 'dashboard#index'
-  
-  # Routes pour les sessions (connexion/déconnexion)
+
+  # Sessions (connexion/déconnexion)
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
+
   # Dashboard
   get '/dashboard', to: 'dashboard#index'
-  
-  # Routes pour les utilisateurs et autres ressources
+
+  # Utilisateurs et autres ressources globales
   resources :users, except: [:destroy]
+
   resources :schools, only: [:index, :show] do
-    resources :information, only: [:index, :new, :create]
+    resources :informations, only: [:index, :new, :create] # correction: "informations"
   end
-  
+
   resources :councils, only: [:index, :show] do
     resources :attendances, only: [:create, :update]
     resources :reports, only: [:new, :create, :edit, :update]
   end
-  
-  resources :information, only: [:show, :edit, :update, :destroy]
+
+  resources :informations, only: [:show, :edit, :update, :destroy]
   resources :reports, only: [:index, :show]
-  
+
   # Espace d'administration
   namespace :admin do
     get '/', to: 'dashboard#index', as: :root
@@ -31,6 +32,8 @@ Rails.application.routes.draw do
     resources :schools
     resources :associations
     resources :councils
+    resources :informations # Ajout de la ressource "informations" en admin
     resources :reports, only: [:index, :show]
   end
 end
+
